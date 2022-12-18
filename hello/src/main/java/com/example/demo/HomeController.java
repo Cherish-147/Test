66f0c4bb90fad52d9f5a7e81743533d919c77e3f
Cherish-147 <120799513+Cherish-147@users.noreply.github.com>
2022-12-18 11:39:50 +0800

    Add files via upload

---
+++ hello/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/hello")
+    String hello(String username, Model model)
+    {
+        model.addAttribute("username", username);
+        return "hello";
+    }
+
+    @RequestMapping("/")
+    String Index()
+    {
+        return "redirect:/books";
+    }
+
+}
---
+++ hello/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import com.example.demo.entities.Book;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    @GetMapping("/test/hello")
+    String Hello(@org.jetbrains.annotations.NotNull Model model)
+    {
+        model.addAttribute("a", 3);
+        model.addAttribute("b", 4);
+        model.addAttribute("c", 5);
+        return "/test/hello";
+    }
+
+    @GetMapping("/test/book")
+    String Book(Model model)
+    {
+        Book book = new Book();
+        book.Id = 123;
+        book.Name = "IBM";
+        model.addAttribute("book", book);
+        return "/test/book";
+    }
+
+
+    @GetMapping("/test/array")
+    String ArrayTest(Model model)
+    {
+        int[] IntArray = {1,2,3,4,5,6,7};
+        model.addAttribute("intArr", IntArray);
+        return "/test/array";
+    }
+}
